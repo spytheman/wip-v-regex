fd29281163d196c79af23b3aec4701d843303296
Delyan Angelov <delian66@gmail.com>
2019-09-30 19:00:28 +0300

    Uncomment the C definitions in lib_pcre.v and work around a v bug.

--- lib_pcre.v
+++ lib_pcre.v
@@ -18,7 +18,7 @@ module regex
 struct C.pcre {}
 struct C.pcre_extra {}
 
-/*
+
 fn C.pcre_compile(byteptr, int, &byteptr, &int, voidptr) &C.pcre
 fn C.pcre_compile2(byteptr, int, &int, &byteptr, &int, byteptr) &C.pcre
 fn C.pcre_copy_named_substring(&C.pcre, byteptr, &int, int, byteptr, byteptr, int) int
@@ -30,7 +30,7 @@ fn C.pcre_fullinfo(&C.pcre, &C.pcre_extra, int, voidptr) int
 fn C.pcre_get_stringnumber(&C.pcre, byteptr) int
 fn C.pcre_get_stringtable_entries(&C.pcre, byteptr, &byteptr, &byteptr) int
 fn C.pcre_get_substring(byteptr, &int, int, int, &byteptr) int
-fn C.pcre_get_substring_list(byteptr, &int, int, &&byteptr) int
+fn C.pcre_get_substring_list(byteptr, &int, int, &byteptr) int
 fn C.pcre_get_named_substring(&C.pctr, byteptr, &int, int, byteptr, &byteptr) int
 fn C.pcre_maketables() byteptr
 fn C.pcre_refcount(&C.pcre, int) int
@@ -39,4 +39,7 @@ fn C.pcre_free_substring_list(&byteptr)
 fn C.pcre_free_substring(byteptr)
 fn C.pcre_free_study(&C.pcre_extra)
 fn C.pcre_free(voidptr)
-*/
+
+fn dummy_func_to_avoid_a_v_bug(){
+}
+
